6c2789e420011bf4ce19d2e83b3f899cf22dba60
刘亮 <iamcrawler@sina.com>
2018-10-26 10:04:00 +0800

    第三次提交

--- src/main/java/cn/iamcrawler/jenkins/Controller.java
+++ src/main/java/cn/iamcrawler/jenkins/Controller.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping
     public String aa(){
-        return "aa";
+        return "aab";
     }
 }
